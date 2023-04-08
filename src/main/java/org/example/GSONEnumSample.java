5c3316bfe0ac6a5926d652ef82daa75faa3c3f21
ing.conti <ingconti@ingconti.com>
2023-04-08 22:29:40 +0200

    2nd commit

--- src/main/java/org/example/GSONEnumSample.java
+++ src/main/java/org/example/GSONEnumSample.java
@@ -4,7 +4,7 @@ package org.example;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class GSONEnumExamples {
+public class GSONEnumSample {
     public static void main(String[] args) {
         serializeEnumGson();
         deserializeEnumGson();
